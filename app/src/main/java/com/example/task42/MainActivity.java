92f5c7b5ae6ae0f8a089e8aa0b817f58d09c11fe
as6620 <as6620@bs.amalnet.k12.il>
2023-12-08 18:48:10 +0200

    button

--- app/src/main/java/com/example/task42/MainActivity.java
+++ app/src/main/java/com/example/task42/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.task42;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View view){
+        btn = findViewById(R.id.button);
+        btn.setText("This is a click number:" );
+
     }
 }
